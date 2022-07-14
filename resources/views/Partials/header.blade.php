40bbc7674dc84dcd1ebbf9f25b07816993755227
Barbara <barbara.cicuto.pereira@gmail.com>
2022-07-14 10:02:59 -0300

    Initiating front-end: header

--- resources/views/Partials/header.blade.php
+++ resources/views/Partials/header.blade.php
@@ -1,3 +1,11 @@
-<header>
-    <a href="{{route('logout')}}">Logout</a>
+<header id="header">
+    <div>
+        <figure>
+            <img src="" title="Logo" alt="agenda's logo">
+        </figure>
+    </div>
+    <nav>
+        <a href="{{route('profile', ['id' => $user_id])}}">Profile</a>
+        <a href="{{route('logout')}}">Logout</a>
+    </nav>
 </header>
--- resources/views/Partials/master.blade.php
+++ resources/views/Partials/master.blade.php
@@ -6,7 +6,9 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Agenda</title>
 
-        <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}"> 
+        <link rel="stylesheet" href="{{ asset('assets/css/header.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
     </head> 
     <body>
         @include('partials.header')

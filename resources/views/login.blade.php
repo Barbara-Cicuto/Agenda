425b1bd4d70beae47fd11c81626416396399cb3f
Barbara <barbara.cicuto.pereira@gmail.com>
2022-08-26 11:58:23 -0300

    .

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,11 +10,11 @@
             @csrf
 
             <label for="email">Email</label><br>
-            <input type="email" name="email" placeholder="Email">
+            <input type="email" name="email" placeholder="Email" class="input">
             <p>{{$errors->has('email') ? $errors->first('email') : ''}}</p>
             
             <label for="password">Password</label><br>
-            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="password" placeholder="Password" class="input">
             <p>{{$errors->has('password') ? $errors->first('password') : ''}}</p>
     
             <input type="checkbox" name="rememberMe">Remember me
